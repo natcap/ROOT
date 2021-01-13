adcba99f39f6fe69583ffe95210378abf0b20700
James Douglass <jamesdouglassusa@gmail.com>
2021-01-13 15:24:07 -0800

    Revert "fixing up setup.py. RE:#1"
    This reverts commit a15568a14534c7c240fa9edadc7236738fb6a637.

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup
 
 setup(
-    name = 'rootcode',
+    name = 'natcap.root',
     use_scm_version={'version_scheme': 'post-release',
                      'local_scheme': 'node-and-date'},
     packages=[
         'rootcode',
     ],
-    package_dir={
-        'rootcode': 'rootcode',
-    },
     setup_requires=['setuptools_scm'],
     zip_safe=False,
 )
