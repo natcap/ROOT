a15568a14534c7c240fa9edadc7236738fb6a637
James Douglass <jamesdouglassusa@gmail.com>
2021-01-13 14:28:20 -0800

    fixing up setup.py. RE:#1

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(
-    name = 'natcap.root',
+    name = 'rootcode',
     use_scm_version={'version_scheme': 'post-release',
                      'local_scheme': 'node-and-date'},
     packages=[
         'rootcode',
     ],
+    package_dir={
+        'rootcode': 'rootcode',
+    },
     setup_requires=['setuptools_scm'],
     zip_safe=False,
 )
