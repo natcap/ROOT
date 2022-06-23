46099a0ff1fdad908336e12f1accd967e24de837
James Douglass <jamesdouglassusa@gmail.com>
2022-06-23 13:13:14 -0500

    Updating GDAL hook to put GDAL *.so in the gdal directory.

--- exe/hooks/hook-osgeo.py
+++ exe/hooks/hook-osgeo.py
@@ -1,18 +1,28 @@
-import os.path
 import glob
+import os.path
+import warnings
 
+from PyInstaller.compat import is_darwin
 from PyInstaller.compat import is_win
-from PyInstaller.utils.hooks import collect_dynamic_libs, get_package_paths
+from PyInstaller.utils.hooks import collect_dynamic_libs
+from PyInstaller.utils.hooks import get_package_paths
 
 if is_win:
-    # GDAL appears to need `_gdal.cp38-win_amd64.pyd` located specifically in
-    # `osgeo/_gdal....pyd` in order to work.  This is because the GDAL python
-    # __init__ script specifically looks in the `osgeo` directory in order to
-    # find it.  This is apparently only an issue on Windows.
-    #
-    # This will take the dynamic libraries in osgeo and put them into osgeo,
-    # relative to the binaries directory.
-    binaries = collect_dynamic_libs('osgeo', 'osgeo')
-    pkg_base, pkg_dir = get_package_paths('osgeo')
-    for pyd_file in glob.glob(os.path.join(pkg_dir, '*.pyd')):
-        binaries.append((pyd_file, 'osgeo'))
+    ext = "pyd"
+elif is_darwin:
+    ext = "so"
+else:
+    warnings.warn(
+        "Building binaries on anything other that windows or mac is untested")
+
+# GDAL appears to need `_gdal.cp38-win_amd64.pyd` located specifically in
+# `osgeo/_gdal....pyd` in order to work.  This is because the GDAL python
+# __init__ script specifically looks in the `osgeo` directory in order to
+# find it.
+#
+# This will take the dynamic libraries in osgeo and put them into osgeo,
+# relative to the binaries directory.
+binaries = collect_dynamic_libs('osgeo', 'osgeo')
+pkg_base, pkg_dir = get_package_paths('osgeo')
+for pyd_file in glob.glob(os.path.join(pkg_dir, f'*.{ext}')):
+    binaries.append((pyd_file, 'osgeo'))
